ebd159b671520a0e846a6ae7f67832cc7ae27e84
Thomas Mansencal <kelsolaar_fool@hotmail.com>
2010-04-25 19:23:34 +0200

    Improve Helper Scripts archiving.

--- utilities/sIBL_GUI_For_Maya_gatherFiles.sh
+++ utilities/sIBL_GUI_For_Maya_gatherFiles.sh
@@ -20,7 +20,7 @@ cp ./src/prefs/icons/*.xpm ./releases/classic/prefs/icons/
 cp ./src/prefs/shelves/shelf_sIBL_GUI.mel ./releases/classic/prefs/shelves/
 cp ./src/scripts/sIBL_GUI_For_Maya.py ./releases/classic/scripts/
 cd ./releases/classic/
-zip ../repository/sIBL_GUI_For_Maya.zip README prefs/shelves/* prefs/icons/* scripts/*
+zip -r ../repository/sIBL_GUI_For_Maya.zip .
 
 #! Reaching Original Directory.
 cd ../../
@@ -34,4 +34,4 @@ cp ./src/prefs/icons/*.png ./releases/qt/prefs/icons/
 cp ./src/prefs/shelves/shelf_sIBL_GUI_Qt.mel ./releases/qt/prefs/shelves/shelf_sIBL_GUI.mel
 cp ./src/scripts/sIBL_GUI_For_Maya_Qt.py ./releases/qt/scripts/sIBL_GUI_For_Maya.py
 cd ./releases/qt/
-zip ../repository/sIBL_GUI_For_Maya_Qt.zip README prefs/shelves/* prefs/icons/* scripts/*
+zip -r ../repository/sIBL_GUI_For_Maya_Qt.zip .
