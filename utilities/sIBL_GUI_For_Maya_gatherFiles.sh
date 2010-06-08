addef84a2e09381a1155af31236ccb7d87ac2f8e
Thomas Mansencal <kelsolaar_fool@hotmail.com>
2010-06-08 20:28:53 +0200

    Update "sIBL_GUI_For_Maya_gatherFiles.sh" gather file verbose.

--- utilities/sIBL_GUI_For_Maya_gatherFiles.sh
+++ utilities/sIBL_GUI_For_Maya_gatherFiles.sh
@@ -4,14 +4,38 @@ echo sIBL_GUI For Maya - Files Gathering
 echo ----------------------------------------------------------------
 
 #! Gathering Folder Cleanup.
+echo ----------------------------------------------------------------
+echo Gathering Folder Cleanup - Begin
+echo ----------------------------------------------------------------
 rm -rf ./releases/classic/*
 rm -rf ./releases/qt/*
 rm -rf ./releases/repository/*
+echo ----------------------------------------------------------------
+echo Gathering Folder Cleanup - End
+echo ----------------------------------------------------------------
+
+#! Extra Files Cleanup.
+echo ----------------------------------------------------------------
+echo Extra Files Cleanup - Begin
+echo ----------------------------------------------------------------
+python ../sIBL_GUI/utilities/sIBL_GUI_recursiveRemove.py ./ .DS_Store
+echo ----------------------------------------------------------------
+echo Extra Files Cleanup - End
+echo ----------------------------------------------------------------
 
 #! Change Log Gathering.
+echo ----------------------------------------------------------------
+echo Change Log Gathering - Begin
+echo ----------------------------------------------------------------
 cp ./releases/Change\ Log.html ./releases/repository/
+echo ----------------------------------------------------------------
+echo Change Log Gathering - End
+echo ----------------------------------------------------------------
 
 #! Classic Gathering.
+echo ----------------------------------------------------------------
+echo Classic Gathering - Begin
+echo ----------------------------------------------------------------
 mkdir -p ./releases/classic/prefs/icons
 mkdir -p ./releases/classic/prefs/shelves
 mkdir ./releases/classic/scripts
@@ -21,10 +45,16 @@ cp ./src/prefs/shelves/shelf_sIBL_GUI.mel ./releases/classic/prefs/shelves/
 cp ./src/scripts/sIBL_GUI_For_Maya.py ./releases/classic/scripts/
 cd ./releases/classic/
 zip -r ../repository/sIBL_GUI_For_Maya.zip .
+echo ----------------------------------------------------------------
+echo Classic Gathering - End
+echo ----------------------------------------------------------------
 
 #! Reaching Original Directory.
 cd ../../
 
+echo ----------------------------------------------------------------
+echo Qt Gathering - Begin
+echo ----------------------------------------------------------------
 #! Qt Gathering.
 mkdir -p ./releases/qt/prefs/icons
 mkdir -p ./releases/qt/prefs/shelves
@@ -35,3 +65,6 @@ cp ./src/prefs/shelves/shelf_sIBL_GUI_Qt.mel ./releases/qt/prefs/shelves/shelf_s
 cp ./src/scripts/sIBL_GUI_For_Maya_Qt.py ./releases/qt/scripts/sIBL_GUI_For_Maya.py
 cd ./releases/qt/
 zip -r ../repository/sIBL_GUI_For_Maya_Qt.zip .
+echo ----------------------------------------------------------------
+echo Qt Gathering - End
+echo ----------------------------------------------------------------
