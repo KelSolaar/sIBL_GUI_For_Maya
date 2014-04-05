97bac50b0b0ca1c5391c5d51376fc363fcd9244f
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-04-05 08:29:16 +0200

    Update "utilities/gatherFiles.sh" file.

--- utilities/gatherFiles.sh
+++ utilities/gatherFiles.sh
@@ -3,20 +3,20 @@ echo ----------------------------------------------------------------
 echo sIBL_GUI For Maya - Files Gathering
 echo ----------------------------------------------------------------
 
-export UTILITIES=/Users/KelSolaar/Documents/Development/sIBL_GUI/utilities
-export PROJECT=/Users/KelSolaar/Documents/Development/sIBL_GUI_For_Maya
+export PROJECT_DIRECTORY=$(cd $( dirname "${BASH_SOURCE[0]}" )/..; pwd)
 
-export SOURCE=$PROJECT/src/
-export RELEASES=$PROJECT/releases/
-export BUILD=$RELEASES/build
-export REPOSITORY=$RELEASES/repository
+export SOURCE_DIRECTORY=$PROJECT_DIRECTORY/src/
+export RELEASES_DIRECTORY=$PROJECT_DIRECTORY/releases/
+export BUILD_DIRECTORY=$RELEASES_DIRECTORY/build
+export REPOSITORY_DIRECTORY=$RELEASES_DIRECTORY/repository
+export UTILITIES_DIRECTORY=$PROJECT_DIRECTORY/utilities
 
 #! Gathering folder cleanup.
 echo ----------------------------------------------------------------
 echo Gathering Folder Cleanup - Begin
 echo ----------------------------------------------------------------
-rm -rf $BUILD
-rm -rf $REPOSITORY/*
+rm -rf $BUILD_DIRECTORY
+rm -rf $REPOSITORY_DIRECTORY/*
 echo ----------------------------------------------------------------
 echo Gathering Folder Cleanup - End
 echo ----------------------------------------------------------------
@@ -25,7 +25,7 @@ echo ----------------------------------------------------------------
 echo ----------------------------------------------------------------
 echo Extra Files Cleanup - Begin
 echo ----------------------------------------------------------------
-python $UTILITIES/recursiveRemove.py ./ .DS_Store
+python $UTILITIES_DIRECTORY/recursiveRemove.py ./ .DS_Store
 echo ----------------------------------------------------------------
 echo Extra Files Cleanup - End
 echo ----------------------------------------------------------------
@@ -34,7 +34,7 @@ echo ----------------------------------------------------------------
 echo ----------------------------------------------------------------
 echo Change Log Gathering - Begin
 echo ----------------------------------------------------------------
-cp $RELEASES/Change_Log.html $REPOSITORY/
+cp $RELEASES_DIRECTORY/Change_Log.html $REPOSITORY_DIRECTORY/
 echo ----------------------------------------------------------------
 echo Change Log Gathering - End
 echo ----------------------------------------------------------------
@@ -42,15 +42,15 @@ echo ----------------------------------------------------------------
 echo ----------------------------------------------------------------
 echo Files Gathering - Begin
 echo ----------------------------------------------------------------
-mkdir -p $BUILD/prefs/icons
-mkdir -p $BUILD/prefs/shelves
-mkdir $BUILD/scripts
-cp $PROJECT/README $BUILD/
-cp $SOURCE/prefs/icons/*.png $BUILD/prefs/icons/
-cp $SOURCE/prefs/shelves/shelf_sIBL_GUI.mel $BUILD/prefs/shelves/shelf_sIBL_GUI.mel
-cp $SOURCE/scripts/sIBL_GUI_For_Maya.py $BUILD/scripts/sIBL_GUI_For_Maya.py
-cd $BUILD/
-zip -r $REPOSITORY/sIBL_GUI_For_Maya.zip .
+mkdir -p $BUILD_DIRECTORY/prefs/icons
+mkdir -p $BUILD_DIRECTORY/prefs/shelves
+mkdir $BUILD_DIRECTORY/scripts
+cp $PROJECT_DIRECTORY/README $BUILD_DIRECTORY/
+cp $SOURCE_DIRECTORY/prefs/icons/*.png $BUILD_DIRECTORY/prefs/icons/
+cp $SOURCE_DIRECTORY/prefs/shelves/shelf_sIBL_GUI.mel $BUILD_DIRECTORY/prefs/shelves/shelf_sIBL_GUI.mel
+cp $SOURCE_DIRECTORY/scripts/sIBL_GUI_For_Maya.py $BUILD_DIRECTORY/scripts/sIBL_GUI_For_Maya.py
+cd $BUILD_DIRECTORY/
+zip -r $REPOSITORY_DIRECTORY/sIBL_GUI_For_Maya.zip .
 echo ----------------------------------------------------------------
 echo Files Gathering - End
 echo ----------------------------------------------------------------
